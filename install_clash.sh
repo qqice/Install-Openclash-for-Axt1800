fae55bfa2ac29d7777548bc27526f7b195eea24b
qqice <qqice@srhaze.com>
2023-02-25 01:58:26 +0800

    Update install_clash.sh

--- install_clash.sh
+++ install_clash.sh
@@ -13,13 +13,16 @@ wget -O /tmp/clash.gz https://cdn.jsdelivr.net/gh/qqice/Install-Openclash-for-Ax
 gzip -d /tmp/clash.gz
 rm -f /etc/openclash/core/clash
 mv /tmp/clash-linux-arm64 /etc/openclash/core/clash
+chmod +x /etc/openclash/core/clash
 
 wget -O /tmp/clash_tun.gz https://cdn.jsdelivr.net/gh/qqice/Install-Openclash-for-Axt1800/assets/clash-linux-arm64-2023.02.16.gz
 gzip -d /tmp/clash_tun.gz
 rm -f /etc/openclash/core/clash_tun
 mv /tmp/clash_tun /etc/openclash/core/clash_tun
+chmod +x /etc/openclash/core/clash_tun
 
 wget -O /tmp/clash_meta.gz https://cdn.jsdelivr.net/gh/qqice/Install-Openclash-for-Axt1800/assets/clash.meta-linux-arm64-v1.14.2.gz
 gzip -d /tmp/clash_meta.gz
 rm -f /etc/openclash/core/clash_meta
 mv /tmp/clash_meta /etc/openclash/core/clash_meta
+chmod +x /etc/openclash/core/clash_meta
